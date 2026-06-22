003a9a4175d77db1549a3785f21a63521a159be6
unknown <abdulgeniabdulaziz@gmail.com>
2026-06-22 23:44:07 +0300

    Minimal FCC format

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors');
 const app = express();
 
-const upload = multer();
-
-app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.json({ error: 'No file' });
-  }
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
@@ -23,4 +16,6 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Running'));
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('App listening on port ' + listener.address().port);
+});
