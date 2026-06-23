689874ee332f887ab9d18af75e62829cc3df7994
unknown <abdulgeniabdulaziz@gmail.com>
2026-06-24 01:31:51 +0300

    Add uploads folder with gitkeep and dotenv

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const app = express();
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
 
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size
-  });
+  })
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('App listening');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
